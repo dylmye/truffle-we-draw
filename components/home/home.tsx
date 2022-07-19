36cf5c8dbce0b23bcf25d578ba957b1c7e6c970a
Dylan Myers <dylmye-github-signed@opayq.com>
2022-07-19 23:37:53 +0100

    use a drawing library instead

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef } from "https://npm.tfl.dev/react";
+import React, { useCallback, useEffect, useRef } from "https://npm.tfl.dev/react";
 import jumper from "https://tfl.dev/@truffle/utils@~0.0.2/jumper/jumper.ts";
 import { useStyleSheet } from "https://tfl.dev/@truffle/distribute@^2.0.0/format/wc/react/index.ts";
+import CanvasDraw from "https://npm.tfl.dev/react-canvas-draw@1";
 import Button from "https://tfl.dev/@truffle/ui@~0.1.0/components/button/button.tag.ts";
 
-import Canvas from "../canvas/canvas.tsx";
 import styleSheet from "./home.scss.js";
 
 function ExtensionMapping() {
   useStyleSheet(styleSheet);
+  const canvasRef = useRef<CanvasDraw | null>(null);
+
+  const onClear = useCallback(() => {
+    canvasRef?.current?.clear();
+  }, []);
 
   useEffect(() => {
     const style = {
@@ -30,9 +35,9 @@ function ExtensionMapping() {
   return (
     <div className="c-home">
       <h1>Prompt: poop</h1>
-      <Canvas />
+      <CanvasDraw canvasWidth={350} canvasHeight={200} brushRadius={5} hideGrid ref={canvasRef} />
       <div className="c-actions">
-        <Button className="button button-clear" onClick={console.log}>Clear</Button>
+        <Button className="button button-clear" onClick={onClear}>Clear</Button>
         <Button className="button button-send" onClick={console.log}>Send</Button>
       </div>
     </div>
